a70d008ab8e5eba4d5c5a3033a675dac4ba30caa
Nichollas Paes <paesnichollas@gmail.com>
2025-07-03 09:40:46 -0300

    add STATIC_ROOT on settings

--- setup/settings.py
+++ setup/settings.py
@@ -123,6 +123,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Media files (uploads)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
